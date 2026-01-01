5099b7852d87e3c1b15eed75a41fd0930e4eabb4
Enoch-Evelyn-10 <Nuckychecksin@gmail.com>
2025-12-31 21:57:38 -0500

    Python-training

---
+++ main.py
@@ -0,0 +1,13 @@
+score = int(input("What is your grade of the exam"))
+
+if score > 90:
+    print ("You got an A") 
+elif score > 80:
+    print ("You got a B")
+elif score > 70:
+    print ("You got a C")
+elif score > 60:
+    print ("you got a D")
+else:
+    print ("You got an F") 
+    
