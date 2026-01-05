a8cfef4bf0d3236bb0809652fada6b05dc8c45b4
Enoch-Evelyn-10 <Nuckychecksin@gmail.com>
2026-01-04 20:09:37 -0500

    training 4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
-#1. score = int(input("What is your grade of the exam"))
+#1. Score Checker
+#  score = int(input("What is your grade of the exam"))
 
 # if score > 90:
 #     print ("You got an A") 
@@ -12,7 +13,8 @@
 # else:
 #     print ("You got an F") 
 
-# 2. Check if a password matches "python123"
+# 2. Password Checker 
+# Check if a password matches "python123"
 
 # password = input("Enter your password" )
 
@@ -21,12 +23,25 @@
 # else:
 #    print("This is inccorect, Access Denied")
 
-# 3. Check user name and password
+# # 3. Username and Password Checker 
+# Check user name and password
 
-username = input("What is your username")
-password  = input("What is your password")
+# username = input("What is your username")
+# password  = input("What is your password")
 
-if username ==" Noxin" and password ==" apple":
-   print("Access Granted")
+# if username ==" Noxin" and password ==" apple":
+#    print("Access Granted")
+# else:
+#    print("Access Denied ")
+
+
+# 4. Number Range Validation 
+# Accepts numbers between 1 and 10 
+
+number = int(input("Enter a number (1-10):"))
+
+# Or operater, allows for one of the statements to be true to produce and true boolean
+if number > 1 or number > 10: 
+    print("correct")
 else:
-   print("Access Denied ")
+    print("incorrect")  
